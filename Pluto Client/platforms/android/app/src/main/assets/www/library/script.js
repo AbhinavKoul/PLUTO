7f2028b7485657c0498d4ff62724159f4f3534fa
unknown <you@example.com>
2020-07-15 23:07:33 +0530

    Added Android App

---
+++ Pluto Client/platforms/android/app/src/main/assets/www/library/script.js
@@ -0,0 +1,8 @@
+const init = localStorage.getItem('init')
+firebase.initializeApp({
+    apiKey: 'AIzaSyBr8auyE3enhMQrZJ7ndiPp7bia_b1mnLE',
+    authDomain: 'pluto-2f203.firebaseapp.com',
+    databaseURL: 'https://pluto-2f203.firebaseio.com',
+    projectId: 'pluto-2f203',
+    appId: '1:1043081025436:web:d04dfc01127e1dfba65a59'
+})
